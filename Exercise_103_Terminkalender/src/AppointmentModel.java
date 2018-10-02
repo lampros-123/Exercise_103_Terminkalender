6089e57a606052860ab8dc5fb21fc5e994307943
lampros <matthias@futurefactory-software.com>
2018-10-02 09:15:33 +0200

    implement load

--- Exercise_103_Terminkalender/src/AppointmentModel.java
+++ Exercise_103_Terminkalender/src/AppointmentModel.java
@@ -1,6 +1,9 @@
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import javax.swing.AbstractListModel;
@@ -34,4 +37,15 @@ public class AppointmentModel extends AbstractListModel<Appointment>{
         }
         oos.close();
     }
+    
+    public void loadAppointements(File f) throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+        try {
+            Object app;
+            while((app = ois.readObject()) != null) {
+                appointments.add((Appointment) app);
+            }
+        } catch(EOFException e) { }
+        ois.close();
+   }
 }
